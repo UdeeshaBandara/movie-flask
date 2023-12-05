2b5675e4a30ec1506a43773a8e9ef3c87941034b
udeeshabandara <udeeshabandara@gmail.com>
2023-12-05 14:25:47 +0530

    mongo url changed

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ import secrets
 secret_key = secrets.token_hex(16)
 
 class Config:
-    MONGO_URI = "mongodb+srv://sktech_123:abcd1234@cluster0.3haue2p.mongodb.net/"
+    MONGO_URI = "mongodb+srv://sktech_123:abcd1234@cluster0.3haue2p.mongodb.net/movie"
     SECRET_KEY = secret_key
 
 app = Flask(__name__)
